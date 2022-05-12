2ce098f8c5e8605bdd81d8f2c6ce858dc1bdda2f
RonanZimmermann <ronan.zimmermann@unibas.ch>
2022-05-12 09:07:25 +0200

    Added docusaurus 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'MHIRA',
   tagline: 'Mental Health Information Reporting Assistant',
   url: 'https://mhira-project.github.io',
-  baseUrl: '/mhira-manual/',
+  baseUrl: '/documentation/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mhira-project', // Usually your GitHub org/user name.
-  projectName: 'mhira-manual', // Usually your repo name.
+  projectName: 'documentation', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mhira-project/mhira-manual',
+            'https://github.com/mhira-project/documentation',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mhira-project/mhira-manual',
+            'https://github.com/mhira-project/documentation',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
